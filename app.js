2882aa1084ca2484d97bb5eb1beba258ec4d6fe2
amnahkhatun <amna.khatun1605@gmail.com>
2021-12-07 15:55:42 +0530

    created a server

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-// require('./addValues')
-const os = require('os');
-const user = os.userInfo();
-console.log("user", user)
+const http = require("http");
 
-const currOSInfo ={
-  name : os.type(),
-  release : os.release(),
-  upTime : (os.uptime()),
-  freeMem : os.freemem(),
-  totalMem : os.totalmem()
+const host = 'localhost';
+const port = 8000;
+
+const cities = ["Mumbai", "London", "New York", "Rome", "Tokyo"];
+
+const getCityName = function () {
+  let city = cities[Math.floor(Math.random() * cities.length)];
+  return city
 }
-console.log("currOSInfo", currOSInfo)
+
+const requestListener = function (req, res) {
+  let message = getCityName();
+  res.setHeader("Content-Type", "application/json");
+  res.writeHead(200);
+  res.end(`{"message": "${message}"}`);
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
---
+++ test2.js
@@ -0,0 +1,16 @@
+const start = async () => {
+  try {
+    const first = await readFile('./content/first.txt', 'utf8')
+    const second = await readFile('./content/second.txt', 'utf8')
+    await writeFile(
+      './content/testing.txt',
+      `THIS IS AWESOME : ${first} ${second}`,
+      { flag: 'a' }
+    )
+    console.log(first, second)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
